1ce966bc98da0d43f82bb2423014fe03ac5e14f6
Yerong <yerong.li@outlook.com>
2023-11-24 16:35:31 -0600

    Use has_noe

--- src/train.py
+++ src/train.py
@@ -12,6 +12,9 @@ import compute_metrics.metric as ms
 from parse_config import ConfigParser
 from model.utils import PPRPowerIteration
 import os
+def print_outgoing_edges(graph, node):
+    for edge in graph.out_edges(node):
+        print(edge)
 
 def find_paths_of_length_2(graph, start_node):
     paths = []
@@ -84,6 +87,7 @@ logging.info('core_graph')
 logging.info(type(core_graph))
 logging.info(core_graph)
 for i in range(300):
+    if core_graph.has_node(i)
     logging.info(find_paths_of_length_2(core_graph,i))
 # for edge in core_graph.edges():
 #     source, target = edge
