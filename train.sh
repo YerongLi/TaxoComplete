b4b5e9e16068d4cd6bd11dd4cc3fe33ce28781ab
Yerong <yerong.li@outlook.com>
2023-11-25 01:13:27 -0600

    Remove the self.root

--- train.sh
+++ train.sh
@@ -1,2 +1,5 @@
 # 500 not working, 3000 works
+# awk '{ if (NR % 10 == 0) print }' wordnet_noun.taxo.bak > wordnet_noun.taxo
+
 python ./src/train.py --config config_files/semeval_noun/config_clst20_s47.json
+awk '{ if (NR % 10 == 0) print }' wordnet_noun.taxo.bak > wordnet_noun.taxo
