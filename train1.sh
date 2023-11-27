427a5c7eddb08683a00e303ecd530a5b609652dd
Yerong <yerong.li@outlook.com>
2023-11-27 06:39:59 -0600

    1500 file

--- train1.sh
+++ train1.sh
@@ -1,4 +1,4 @@
 # 500 not working, 3000 works
 # awk '{ if (NR % 20 == 0) print }' wordnet_noun.taxo.bak > wordnet_noun.taxo
 
-python ./src/train1.py --config config_files/semeval_noun1/config_700.json
+python ./src/train1.py --config config_files/semeval_noun1/config_1500.json
