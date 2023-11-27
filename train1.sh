9ae0484ed4d909e85b84241941eb15095fce3d21
Yerong <yerong.li@outlook.com>
2023-11-27 04:37:43 -0600

    Rename configuration file

--- train1.sh
+++ train1.sh
@@ -1,4 +1,4 @@
 # 500 not working, 3000 works
 # awk '{ if (NR % 20 == 0) print }' wordnet_noun.taxo.bak > wordnet_noun.taxo
 
-python ./src/train1.py --config config_files/semeval_noun1/config_clst20_s47.json
+python ./src/train1.py --config config_files/semeval_noun1/config_300.json
