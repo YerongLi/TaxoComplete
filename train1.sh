fa010c136730e2170ba7281d666f1df4193936ac
Yerong <yerong.li@outlook.com>
2023-12-02 04:31:49 -0600

    Update the train1.sh

--- train1.sh
+++ train1.sh
@@ -2,6 +2,6 @@
 # awk '{ if (NR % 20 == 0) print }' wordnet_noun.taxo.bak > wordnet_noun.taxo
 
 # python ./src/train1.py --config config_files/semeval_noun1/config_1500.json
-# python ./src/train1.py 700 --config config_files/semeval_noun/config_clst20_s47.json
-python ./src/train1.py --config config_files/cs/config_clst20_s47.json 500
+python ./src/train1.py --config config_files/semeval_noun/config_clst20_s47.json 300
+# python ./src/train1.py --config config_files/cs/config_clst20_s47.json 500
 
